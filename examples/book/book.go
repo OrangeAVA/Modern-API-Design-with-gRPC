3755fa63c204c11a7397beca2e3ecd7161614ba1
hiteshrepo <pattanayak.hitesh03@gmail.com>
2023-09-03 11:28:29 +0530

    chapter-2 | seriallize book info in hexadecimal

--- examples/book/book.go
+++ examples/book/book.go
@@ -1,8 +1,27 @@
 package book
 
+import (
+	"encoding/hex"
+
+	"google.golang.org/protobuf/proto"
+)
+
 func NewBook(isbn, publisher string) *BookInfo {
 	return &BookInfo{
 		Isbn:      isbn,
 		Publisher: publisher,
 	}
+}
+
+func SeriallizeBookInfoInHexaDecimal(isbn, publisher string) (string, error) {
+	b := NewBook(isbn, publisher)
+
+	serializedBook, err := proto.Marshal(b)
+	if err != nil {
+		return "", err
+	}
+
+	hexString := hex.EncodeToString(serializedBook)
+
+	return hexString, nil
 }
--- examples/book/book_test.go
+++ examples/book/book_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/HiteshRepo/Modern-API-Design-with-gRPC/examples/book"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func Test_NewBook(t *testing.T) {
@@ -12,4 +13,11 @@ func Test_NewBook(t *testing.T) {
 
 	assert.Equal(t, "1234", b.GetIsbn())
 	assert.Equal(t, "Ava Orange", b.GetPublisher())
+}
+
+func Test_SeriallizeBookInfoInHexaDecimal(t *testing.T) {
+	hex, err := book.SeriallizeBookInfoInHexaDecimal("1234", "Ava Orange")
+
+	require.NoError(t, err)
+	assert.Equal(t, "0a0431323334120a417661204f72616e6765", hex)
 }
