59ae0406a554c62297cdfe3d4838f36f097c3e1d
hiteshrepo <pattanayak.hitesh03@gmail.com>
2023-11-14 00:45:38 +0530

    chapter-5 | resiliency | circuit breaker pattern

--- books-app/internal/apps/grpc-book-info-server/handlers.go
+++ books-app/internal/apps/grpc-book-info-server/handlers.go
@@ -2,7 +2,9 @@ package grpcbooksserver
 
 import (
 	"context"
+	"fmt"
 	"log"
+	"time"
 
 	"github.com/HiteshRepo/Modern-API-Design-with-gRPC/books-app/internal/pkg/proto"
 )
@@ -10,14 +12,44 @@ import (
 func (a *App) GetBookInfoWithReviews(ctx context.Context, req *proto.GetBookInfoRequest) (*proto.GetBookInfoResponse, error) {
 	log.Println("fetching book and reviews")
 
-	book, err := a.bookServerClient.GetBook(ctx, &proto.GetBookRequest{Isbn: req.GetIsbn()})
+	// Set a deadline for the entire operation.
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	// Define a function for getting a book.
+	getBookFunc := func() (interface{}, error) {
+		return a.bookServerClient.GetBook(ctx, &proto.GetBookRequest{Isbn: req.GetIsbn()})
+	}
+
+	// Execute getBookFunc function using the circuit breaker.
+	result, err := a.cb.Execute(ctx, getBookFunc)
+
 	if err != nil {
 		return nil, err
 	}
 
-	resp, err := a.reviewServerClient.GetBookReviews(ctx, &proto.GetBookReviewsRequest{Isbn: req.GetIsbn()})
+	// Display the result.
+	book, ok := result.(*proto.Book)
+	if !ok {
+		return nil, fmt.Errorf("could not fetch book with isbn(%v)", req.GetIsbn())
+	}
+
+	// Define a function for getting book reviews.
+	getBookReviewsFunc := func() (interface{}, error) {
+		return a.reviewServerClient.GetBookReviews(ctx, &proto.GetBookReviewsRequest{Isbn: req.GetIsbn()})
+	}
+
+	// Execute getBookReviewsFunc function using the circuit breaker.
+	result, err = a.cb.Execute(ctx, getBookReviewsFunc)
+
 	if err != nil {
 		return nil, err
+	}
+
+	// Display the result.
+	resp, ok := result.(*proto.GetBookReviewsResponse)
+	if !ok {
+		return nil, fmt.Errorf("could not fetch book reviews with isbn(%v)", req.GetIsbn())
 	}
 
 	return &proto.GetBookInfoResponse{
