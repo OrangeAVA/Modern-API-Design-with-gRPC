831d6157fae7da5915b7de411fd0a6132dffa7b5
hiteshrepo <pattanayak.hitesh03@gmail.com>
2023-11-25 00:33:23 +0530

    chapter-6 | integrate loadbalancing with greet client

--- loadbalancing/internal/app/greetclient/app.go
+++ loadbalancing/internal/app/greetclient/app.go
@@ -7,15 +7,26 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/HiteshRepo/Modern-API-Design-with-gRPC/loadbalancing/internal/pkg/kubernetes"
 	"github.com/HiteshRepo/Modern-API-Design-with-gRPC/loadbalancing/internal/pkg/proto"
+	lapb "github.com/HiteshRepo/Modern-API-Design-with-gRPC/loadbalancing/internal/pkg/proto/lookaside"
 	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
 )
 
 type App struct {
-	client proto.GreetServiceClient
-	conn   *grpc.ClientConn
+	greetClients       map[string]proto.GreetServiceClient
+	conn               map[string]*grpc.ClientConn
+	currentGreetClient proto.GreetServiceClient
+
+	lookasideConn   *grpc.ClientConn
+	lookasideClient lapb.LookasideClient
+}
+
+func NewGreetClient() *App {
+	return &App{
+		greetClients: make(map[string]proto.GreetServiceClient),
+		conn:         make(map[string]*grpc.ClientConn),
+	}
 }
 
 type GreetingRequest struct {
@@ -24,9 +35,9 @@ type GreetingRequest struct {
 }
 
 func (a *App) Start() {
-	err := a.setupGreetClient()
+	err := a.setupLookasideClient()
 	if err != nil {
-		log.Printf("could not setup greet client: %v", err)
+		log.Printf("could not setup lookaside client: %v", err)
 		return
 	}
 
@@ -45,63 +56,106 @@ func (a *App) Start() {
 	router.Run(fmt.Sprintf("0.0.0.0:%s", port))
 }
 
-func (a *App) setupGreetClient() error {
-	var err error
-
+func (a *App) setupGreetClient(host string) error {
 	fmt.Println("Starting greet client")
 
 	opts := grpc.WithInsecure()
-	serverHost := "localhost"
 	serverPort := "50051"
 
 	if port, ok := os.LookupEnv("GRPC_SERVER_PORT"); ok {
 		serverPort = port
 	}
 
-	client, err := kubernetes.GetClusterClient()
-	if err != nil {
-		fmt.Printf("error while creating client: %s\n", err.Error())
+	if c, ok := a.greetClients[host]; !ok {
+		servAddr := fmt.Sprintf("%s:%s", host, serverPort)
+
+		fmt.Println("dialing greet server", servAddr)
+
+		conn, err := grpc.Dial(
+			servAddr,
+			opts,
+		)
+		if err != nil {
+			log.Printf("could not connect greet server: %v", err)
+			return err
+		}
+
+		a.conn[host] = conn
+
+		a.currentGreetClient = proto.NewGreetServiceClient(conn)
+		a.greetClients[host] = a.currentGreetClient
+	} else {
+		a.currentGreetClient = c
 	}
 
-	if host := kubernetes.GetServiceDnsName(client, os.Getenv("GRPC_SVC"), os.Getenv("POD_NAMESPACE")); len(host) > 0 {
-		serverHost = host
+	return nil
+}
+
+func (a *App) setupLookasideClient() error {
+	var err error
+
+	fmt.Println("Starting lookaside client")
+
+	opts := grpc.WithInsecure()
+	serverPort := "50055"
+
+	if port, ok := os.LookupEnv("LB_PORT"); ok {
+		serverPort = port
 	}
 
-	servAddr := fmt.Sprintf("%s:%s", serverHost, serverPort)
+	servAddr := fmt.Sprintf("%s:%s", os.Getenv("LB_SVC_NAME"), serverPort)
 
-	fmt.Println("dialing", servAddr)
+	fmt.Println("dialing lookaside server", servAddr)
 
-	a.conn, err = grpc.Dial(
+	a.lookasideConn, err = grpc.Dial(
 		servAddr,
-		grpc.WithDefaultServiceConfig(`{"loadBalancingPolicy":"round_robin"}`),
-		grpc.WithBlock(),
 		opts,
 	)
 	if err != nil {
-		log.Printf("could not connect: %v", err)
+		log.Printf("could not connect lookaside server: %v", err)
 		return err
 	}
 
-	a.client = proto.NewGreetServiceClient(a.conn)
+	a.lookasideClient = lapb.NewLookasideClient(a.lookasideConn)
 
 	return nil
 }
 
 func (a *App) Shutdown() {
-	a.conn.Close()
+	for _, conn := range a.conn {
+		conn.Close()
+	}
 }
 
 func (a *App) doUnary(firstName, lastName string) string {
+	r, err := a.lookasideClient.Resolve(context.Background(), &lapb.Request{
+		Router:    lapb.Request_ROUNDROBIN,
+		Service:   os.Getenv("GRPC_SVC"),
+		Namespace: os.Getenv("POD_NAMESPACE"),
+	})
+
+	if err != nil {
+		log.Printf("could not resolve greet server address: %v", err)
+		return ""
+	}
+
+	err = a.setupGreetClient(r.GetAddress())
+	if err != nil {
+		log.Printf("could not setup greet client: %v", err)
+		return ""
+	}
+
 	req := &proto.GreetingRequest{
 		Greeting: &proto.Greeting{
 			FirstName: firstName,
 			LastName:  lastName,
 		},
 	}
-	res, err := a.client.Greet(context.Background(), req)
+	res, err := a.currentGreetClient.Greet(context.Background(), req)
 	if err != nil {
 		log.Fatalf("error while calling greet rpc : %v", err)
 	}
+
 	return fmt.Sprintf("reponse from Greet rpc: %v", res.Result)
 }
 
